14d273992b88571e0e5929e6144f662ee8daffa4
KanwalShehzadi98-ux <kanwalshehz64@gmail.com>
2024-02-16 22:13:50 +0500

    First Commit

---
+++ OOP/First Class.py
@@ -0,0 +1,11 @@
+class Person:
+    def __init__(self,first_name,last_name,age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+p1 = Person("Kanwal","Shehzadi",21)
+p2 = Person("Aown","Abbas",12)
+ 
+print(p1.first_name)
+print(p2.first_name)
